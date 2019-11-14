1e9863513ccdbc13f66a0898d192b695801485ff
Ankith Ede <ankithe@gmail.com>
2019-11-14 12:00:55 -0500

    no p2

--- proc.c
+++ proc.c
@@ -353,29 +353,29 @@ struct proc *p2;
       int maxQueue = 0;
     
       //adjust the queue level for each process and get the maxQueue
-      for (p2 = ptable.proc; p2 < &ptable.proc[NPROC]; p2++){
+      for (p = ptable.proc; p < &ptable.proc[NPROC]; p++){
         int queueIterations[4] = {500, 24, 16, 8};
 
 //p2->idleCount * 10 >= queueIterations[p2->queueNum] &&
         
         //check idle count and move up to avoid starvation
-        if ( p2->queueNum < 3)
+        if ( p->queueNum < 3)
         {
-          p2->queueNum++;
-          p2->idleCount = 0;
-          p2->iterationsLeft = queueIterations[p2->queueNum];
+          p->queueNum++;
+          p->idleCount = 0;
+          p->iterationsLeft = queueIterations[p->queueNum];
         }
 
-        p2->idleCount++;
+        p->idleCount++;
         
 
        
         //check iterations left to decrease queue
-        if (p2->iterationsLeft <= 0)
+        if (p->iterationsLeft <= 0)
         {
-          p2->queueNum--;
-          p2->idleCount = 0;
-          p2->iterationsLeft = queueIterations[p2->queueNum];
+          p->queueNum--;
+          p->idleCount = 0;
+          p->iterationsLeft = queueIterations[p->queueNum];
         } 
         
         /*
@@ -395,9 +395,9 @@ struct proc *p2;
 
 
         //update maxQueue
-        if (p2->queueNum > maxQueue)
+        if (p->queueNum > maxQueue)
         {
-          maxQueue = p2->queueNum;
+          maxQueue = p->queueNum;
         }
       } 
 
